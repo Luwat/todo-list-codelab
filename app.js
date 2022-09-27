cc2559824aac2099c14f9f1d770b2ca9067bc04c
luwat <oluwatobioreniyi@gmail.com>
2022-09-27 08:13:52 +0100

    create append task div

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const inputTask = document.querySelector('.input-task');
 const addBtn = document.querySelector('.add-task');
-const taskContainer = document.querySelector('.task-container'); 
+const taskContainer = document.querySelector('.task-container');
+
+addBtn.addEventListener('click', function() {
+    const task = document.createElement('div');
+    task.classList.add('task');
+
+    const li = document.createElement('li');
+    li.innerHTML = `${inputTask.value}`;
+    task.append(li);
+
+    if (inputTask.value === '') {
+        alert('Field cannot be empty!');
+    } else {
+        taskContainer.append(task);
+    }
+
+    inputTask.value = '';
+})
